6ef8764242594ce1e146ddd2f9017bc40f4bf0fc
Metal450 <metal450@gmail.com>
2025-04-19 16:33:47 -0700

    Unused imports

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/searchfilter/TransactionSearchField.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/searchfilter/TransactionSearchField.java
@@ -1,11 +1,7 @@
 package name.abuchen.portfolio.ui.util.searchfilter;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
